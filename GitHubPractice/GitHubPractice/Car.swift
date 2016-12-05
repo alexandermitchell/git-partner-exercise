73f8291b761bbe4efae8e6c2f0099107b069efbd
Alex Mitchell <alex.mitchell@mac.com>
2016-12-05 16:28:21 -0500

    Created new project

---
+++ GitHubPractice/GitHubPractice/Car.swift
@@ -0,0 +1,16 @@
+//
+//  Car.swift
+//  GitHubPractice
+//
+//  Created by Alex Mitchell on 2016-12-05.
+//  Copyright © 2016 Alex Mitchell. All rights reserved.
+//
+
+import Foundation
+
+
+class Car {
+    
+    
+    
+}
